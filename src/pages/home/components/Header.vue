43fb9b68209b0ed6c4467c38461d18fb99fffc41
胡日查 <1270660449@qq.com>
2018-06-07 08:18:32 +0800

    header-over

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
 	<div class="header">
-		<div class="header-left">返回</div>
-		<div class="header-input">输入城市/景点/游玩主题</div>
-		<div class="header-right">城市</div>
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xe624;</div>
+		</div>
+		<div class="header-input">
+		<span class="iconfont">&#xe632;</span>
+		输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">城市
+		<span class="iconfont arrow-icon">&#xe64a;</span>
+		</div>
 	</div>
 </template>
 
@@ -12,21 +19,29 @@
 	}
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+// @import '../../../assets/styles/varibles.styl'
+// @import '~@/assets/styles/varibles.styl'
+// @指向src目录,样式里引入样式前面加~,import前面加@
+@import '~styles/varibles.styl'
 .header
 	display:flex
 	line-height: .86rem
-	background:#00bcd4
+	background:$bgColor
 	color:#fff
 	.header-left
 		width: .64rem
 		float:left
+		.back-icon
+			text-align:center
+			font-size: .4rem
 	.header-input
 		flex:1
 		height: .64rem
 		line-height: .64rem
 		margin-top: .12rem
-		marginleft: .2rem
+		margin-left: .2rem
+		padding-left: .2rem
 		background: #fff
 		border-radius: .1rem
 		color: #ccc
@@ -34,6 +49,9 @@
 		width: 1.24rem
 		float:right
 		text-align:center
+		.arrow-icon
+			margin-left:-.04rem
+			font-size: .24rem
 </style>
 
 <!-- 1rem 50px -->
