314a544e14251899665651f86549e5c6c3f414ac
胡日查 <1270660449@qq.com>
2018-06-08 16:23:08 +0800

    cdimg

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -25,18 +25,18 @@
 				recommendList:[{
 					id:'0001',
 					imgUrl:'http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg',
-					title:'武汉海昌极地海洋世界',
-					desc:'武汉第一站,浪漫的海洋主题乐园'
+					title:'武汉必游TOP10',
+					desc:'到武汉感受神奇的热干面之都'
 				},{
 					id:'0002',
-					imgUrl:'http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg',
-					title:'武汉海昌极地海洋世界',
-					desc:'武汉第一站,浪漫的海洋主题乐园'
+					imgUrl:'http://img1.qunarzz.com/sight/source/1505/4f/95a94c90ef3521.jpg_r_640x214_345412f9.jpg',
+					title:'开启亲子时光',
+					desc:'这些地方都是宝贝的最爱'
 				},{
 					id:'0003',
-					imgUrl:'http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg',
-					title:'武汉海昌极地海洋世界',
-					desc:'武汉第一站,浪漫的海洋主题乐园'
+					imgUrl:'http://img1.qunarzz.com/sight/source/1505/3a/f96e254541abb.jpg_r_640x214_6b1fc9f0.jpg',
+					title:'鄂西旅游圈',
+					desc:'感受鄂西旖旎风光，土苗民族风情'
 				}]
 				}
 		}
