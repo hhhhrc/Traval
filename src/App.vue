ec2475ad1f367ced1ac30362296ee426c0e23066
胡日查 <1270660449@qq.com>
2018-06-11 15:53:27 +0800

    keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-
-    <router-view/>
+	<keep-alive>
+    	<router-view/>
+	</keep-alive>
+	<!-- 路由中的内容加载过一次之后就放入内存之中,无需再次渲染 -->
     <!-- 当前显示内容是路由对应组件 -->
   </div>
 </template>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,42 +15,55 @@
 	import HomeIcons from './components/Icons'
 	import HomeRecommend from './components/Recommend'
 	import HomeWeekend from './components/Weekend'
+	import {mapState} from 'vuex'
 	export default{
-		name:'Home',
-		components:{
-			HomeHeader,
-			HomeSwiper,
-			HomeIcons,
-			HomeRecommend,
-			HomeWeekend
-		},
-		data () {
-				return {
-					swiperList: [],
-					iconList:[],
-					recommendList:[],
-					weekendList:[]
-				}
-		},
-		methods:{
-			getHomeInfo () {
-				axios.get('/api/index.json')
-					.then(this.getHomeInfoSucc)
-			},
-			getHomeInfoSucc(res){
-				res=res.data
-				if(res.ret&&res.data){
-					const data=res.data
-					this.swiperList=data.swiperList
-					this.iconList=data.iconList
-					this.recommendList=data.recommendList
-					this.weekendList=data.weekendList
-				}
+	name:'Home',
+	components:{
+		HomeHeader,
+		HomeSwiper,
+		HomeIcons,
+		HomeRecommend,
+		HomeWeekend
+	},
+	data () {
+			return {
+				lastCity:'',
+				swiperList: [],
+				iconList:[],
+				recommendList:[],
+				weekendList:[]
 			}
+	},
+	computed:{
+		...mapState(['city'])
+	},
+	methods:{
+		getHomeInfo () {
+			axios.get('/api/index.json?city=' + this.city)
+				.then(this.getHomeInfoSucc)
 		},
-		mounted ( ) {
-			this.getHomeInfo( )
+		getHomeInfoSucc(res){
+			res=res.data
+			if(res.ret&&res.data){
+				const data=res.data
+				this.swiperList=data.swiperList
+				this.iconList=data.iconList
+				this.recommendList=data.recommendList
+				this.weekendList=data.weekendList
+			}
 		}
+	},
+	mounted ( ) {
+		this.lastCity=this.city
+		this.getHomeInfo( )
+	},
+	activated(){
+		if(this.lastCity !== this.city){
+			this.lastCity=this.city
+			this.getHomeInfo()
+		}
+	}
+	// keepalive新增的生命周期函数,页面重新加载时执行.
 	}
 
 	// ajax->父组件(在父组件中定义新数据)->通过属性形式传给子组件->子组件接收(固定类型)
